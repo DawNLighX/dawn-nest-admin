f3673bf8035422baf487e63884a4aafcfdd2d820
DawNLighX <dawnlighx@gmail.com>
2026-01-15 17:43:32 +0800

    feat: 新增book模块及相关控制器 feat: 新增exception过滤器，优化错误处理机制 feat: 新增auth鉴权模块及相关控制器 feat: 新增user用户模块及相关控制器

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
+import { Controller, Get, Post, Put, Patch, Delete, Param, Query, Body, UseFilters} from '@nestjs/common';
 import { AppService } from './app.service';
+import { HttpExceptionFilter } from './exception/http-exception.filter';
 
-@Controller()
+@Controller() // Default route controller 路由请求控制器
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/')
+  @UseFilters(new HttpExceptionFilter())
   getHello(): string {
     return this.appService.getHello();
   }
+
 }
